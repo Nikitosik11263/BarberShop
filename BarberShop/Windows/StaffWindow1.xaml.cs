d3af643a7c113e18d0f5927286a1247153873d77
Nikitosik11263 <80972952+Nikitosik11263@users.noreply.github.com>
2022-01-14 16:42:16 +0300

    Добавьте файлы проекта.

---
+++ BarberShop/Windows/StaffWindow1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BarberShop.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для StaffWindow1.xaml
+    /// </summary>
+    public partial class StaffWindow1 : Window
+    {
+        public StaffWindow1()
+        {
+            InitializeComponent();
+        }
+    }
+}
